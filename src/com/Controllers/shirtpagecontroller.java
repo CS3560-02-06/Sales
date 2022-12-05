8b6c8b2e67a284ad4ac66072e13e30e58c258ba8
Kayla <85607915+kltang6328@users.noreply.github.com>
2022-12-05 03:39:00 -0800

    Added some functionality to Add to Cart button

--- src/com/Controllers/shirtpagecontroller.java
+++ src/com/Controllers/shirtpagecontroller.java
@@ -6,11 +6,15 @@ import com.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class shirtpagecontroller {
 
+    private int num;
     @FXML
-    private Button shirt;
+    private Button addToCart;
+    @FXML
+    private Label cartNum;
     
     public shirtpagecontroller() {
 
@@ -19,5 +23,10 @@ public class shirtpagecontroller {
     public void SelectSearch(ActionEvent event) throws IOException {
         Main m = new Main();
         m.ChangeScene("com/mainpage.fxml");
+    }
+
+    public void SelectAddToCart(ActionEvent event) throws IOException {
+        num++;
+        cartNum.setText(""+num);
     }
 }
